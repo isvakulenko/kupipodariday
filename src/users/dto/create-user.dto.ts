06266d40415dd0bf3eb129e91b5c1eb32531f1a8
isvakulenko <ivan-vakulenko@yandex.ru>
2023-02-05 21:54:53 +0300

    Add DTO for User

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,14 @@
-export class CreateUserDto {}
+import { Length, IsUrl, IsEmail } from 'class-validator';
+
+export class CreateUserDto {
+  @Length(2, 30)
+  username: string;
+  @Length(2, 200)
+  about?: string;
+  @IsUrl()
+  avatar?: string;
+  @IsEmail()
+  email: string;
+  @Length(3, 10)
+  password: string;
+}
--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,4 +1,16 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
+import { Length, IsUrl, IsEmail } from 'class-validator';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto extends PartialType(CreateUserDto) {
+  @Length(2, 30)
+  username?: string;
+  @Length(2, 200)
+  about?: string;
+  @IsUrl()
+  avatar?: string;
+  @IsEmail()
+  email: string;
+  @Length(3, 10)
+  password?: string;
+}
